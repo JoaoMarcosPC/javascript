cd910d92311ee303c75cd234ee9d68cdb222fc81
João Marcos P Cavalcante <JMpeixotoC@hotmail.com>
2022-03-07 09:47:01 -0300

    added variables

---
+++ content/1_basics.js
@@ -0,0 +1,9 @@
+// This is a comment, and bellow it, there is a functions that prints something on the console
+console.log("Hola mundo");
+
+// This one creates a popup
+window.alert("Popup")
+
+/*
+  Multiline comment
+*/
---
+++ content/2_variables.js
@@ -0,0 +1,25 @@
+/* -----VARIABLES-----
+
+  -> In JS you can declare a variable by using: let, const, var
+  -> Don't use var, variables declared with var have a global scope, this isn't exactly bad, but is a good practice not to do it, instead, use let (has local scope)
+  -> If you want a variable to have constant value throughout the program, use const
+*/
+
+/* -----DATA TYPES-----
+
+  -> In JS you don't need to declare a data type to an variable, it does so automatically
+
+  -> Data Types:
+    - string: "This is a string"
+    - numbers: 43
+    - boolean: true or false
+*/
+
+var number; // You can declare a variable without assigning a value
+let animal = "Lion";
+const isAlie = true;
+
+number = 56;
+
+console.log(number + 22);
+console.log("Look there's a", animal, "over there!");
