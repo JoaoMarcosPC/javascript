0a6c678d67d9ee652732ecbc8acd46f235fddb8a
João Marcos P Cavalcante <JMpeixotoC@hotmail.com>
2022-03-07 16:32:58 -0300

    added arithmetic

---
+++ 3_arithmetic_logic.js
@@ -0,0 +1,19 @@
+/*-----ARITHMETIC LOGIC-----
+
+  -> Arithmetic operators: +, -, *, /, %(return the remainder of the division)
+  -> Logic operators:
+
+  -> Operator precedence:
+    1. ()
+    2. exponents
+    3. division & multiplication
+    4. addition & subtraction 
+*/
+
+const x = 9
+let y = x + 6
+y = y + 1
+y -= 1
+y = (5 - 7) * y
+
+console.log(y)
--- content/2_variables.js
+++ content/2_variables.js
@@ -1,11 +1,11 @@
-/* -----VARIABLES-----
+/*-----VARIABLES-----
 
   -> In JS you can declare a variable by using: let, const, var
   -> Don't use var, variables declared with var have a global scope, this isn't exactly bad, but is a good practice not to do it, instead, use let (has local scope)
   -> If you want a variable to have constant value throughout the program, use const
 */
 
-/* -----DATA TYPES-----
+/*-----DATA TYPES-----
 
   -> In JS you don't need to declare a data type to an variable, it does so automatically
 
